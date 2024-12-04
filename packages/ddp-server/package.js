5fcf8e6d99a182a6dca3a76ac107ed9ba78cb462
Leonardo Venturini <leovenbag@gmail.com>
2024-12-04 10:54:36 -0400

    update permessage-deflate2

--- packages/ddp-server/package.js
+++ packages/ddp-server/package.js
@@ -5,7 +5,7 @@ Package.describe({
 });
 
 Npm.depends({
-  "permessage-deflate2": "0.1.8-alpha.1",
+  "permessage-deflate2": "0.1.8",
   sockjs: "0.3.24",
   "lodash.once": "4.1.1",
   "lodash.isempty": "4.4.0",
--- packages/socket-stream-client/package.js
+++ packages/socket-stream-client/package.js
@@ -7,7 +7,7 @@ Package.describe({
 
 Npm.depends({
   "faye-websocket": "0.11.4",
-  "permessage-deflate2": "0.1.8-alpha.1",
+  "permessage-deflate2": "0.1.8",
   "lodash.isequal": "4.5.0",
   "lodash.once": "4.1.1"
 });
