f0a747d5956cdc42dff5ea91b0e7077326eb89e5
Nacho Codoñer <igcogi@gmail.com>
2025-04-30 18:15:10 +0200

    fix tests

--- tools/cli/commands.js
+++ tools/cli/commands.js
@@ -580,7 +580,6 @@ async function doRunCommand(options) {
   webArchs = filterWebArchs(webArchs, options['exclude-archs'], options.appDir, options);
   // Set the webArchs to include for compilation later
   global.includedWebArchs = webArchs;
-  global.modernWatcher = isModernWatcherEnabled(options.appDir);
 
   const buildMode = options.production ? 'production' : 'development';
 
@@ -1481,7 +1480,6 @@ on an OS X system.");
       ...options,
       platforms: projectContext.platformList.getPlatforms(),
     });
-    global.modernWatcher = isModernWatcherEnabled(options.appDir);
   }
 
   var buildDir = projectContext.getProjectLocalDirectory('build_tar');
@@ -2552,7 +2550,6 @@ var runTestAppForPackages = async function (projectContext, options) {
   buildOptions.webArchs = filterWebArchs(webArchs, options['exclude-archs'], projectContext.appDirectory, options);
   // Set the webArchs to include for compilation later
   global.includedWebArchs = buildOptions.webArchs;
-  global.modernWatcher = isModernWatcherEnabled(projectContext.appDirectory);
 
   if (options.deploy) {
     // Run the constraint solver and build local packages.
