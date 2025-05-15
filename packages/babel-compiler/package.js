4576e6938aaab953a722a4f939bad845c0949272
italo jose <italo.i@live.com>
2025-05-15 10:45:15 -0300

    Meteor version to 3.3-beta.1 :comet:

--- packages/babel-compiler/package.js
+++ packages/babel-compiler/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "babel-compiler",
   summary: "Parser/transpiler for ECMAScript 2015+ syntax",
-  version: '7.12.0-beta330.0',
+  version: '7.12.0-beta330.1',
 });
 
 Npm.depends({
