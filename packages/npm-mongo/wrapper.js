48c5975f3987c63584928dda3b49c46607ea479a
italo jose <italo.i@live.com>
2025-07-14 14:35:55 -0300

    feat: choosing between mongo legacy or newest version

--- packages/npm-mongo/wrapper.js
+++ packages/npm-mongo/wrapper.js
@@ -1,4 +1,23 @@
-const useLegacyMongo = !!Package['npm-mongo-legacy'];
+const { MongoClient, MongoServerSelectionError, MongoCompatibilityError } = Npm.require('mongodb');
+
+async function ableToConnectToMongo(url) {
+  let client;
+  try {
+    client = new MongoClient(url);
+    await client.connect();
+  } catch (error) {
+    if (error.cause instanceof MongoCompatibilityError && error.message.includes('maximum wire version')) {
+      console.warn('Legacy MongoDB version detected, using mongo-legacy package:', error.message);
+      console.warn('Warning: MongoDB versions <= 3.6 are deprecated. Some Meteor features may not work properly with this version. It is recommended to use MongoDB >= 4.');
+    } else console.error('Failed to get MongoDB server version:', error);
+    return false;
+  } finally {
+    if (client) await client.close().catch(() => {});
+    return true
+  }
+}
+
+const useLegacyMongo = ableToConnectToMongo(process.env.MONGO_URL)
 
 const oldNoDeprecationValue = process.noDeprecation;
 try {
